9cdc7a2050d0705e3b52354dee93f8a49aeeeaf6
had <david.hauri@imis.ch>
2015-02-27 16:32:49 +0100

    waste another 90' just to prepare setup before beginning with hw, yay

---
+++ 20150223_vl/HDynFileReader.c
@@ -0,0 +1,27 @@
+/* (C) Hauronen Patronens waste of time projects!
+* https://github.com/chubbson/zhaw_os_linux
+* Author: David Hauri
+* Date: 2014-02-27
+* License: GPL v2 (See https://de.wikipedia.org/wiki/GNU_General_Public_License )
+*/
+
+// less oder more schreiben. 
+// zeile vor, zeile zurück gehen. 
+// next char
+// prev char
+
+// direkt auf tastendruck reagieren, nicht zuerst taste dann return!
+// setbuff, setvbuff
+
+// int x;
+// scanf("%d",&x)
+
+//strcmp gute approx für utf-8
+// wandet byteseq der versch. encodings - iconv
+
+// od -x old.utf8.txt | head -20
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
