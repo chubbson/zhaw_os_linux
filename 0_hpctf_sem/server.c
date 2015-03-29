ea73b1d3e8953e102d7005cb3a6c163262a13798
had <david.hauri@imis.ch>
2015-03-29 17:43:32 +0200

    testwise thread based sample clients added

--- 0_hpctf_sem/server.c
+++ 0_hpctf_sem/server.c
@@ -60,6 +60,160 @@ int take(fldstruct *fs, int y, int x, int player)
 	return 1;
 }
 
+struct thread_info {     // Used as argument to thread_start() 
+  pthread_t thread_id;   // ID returned by pthread_create() 
+  int       thread_num;  // Application-defined thread # 
+  int     	player;
+  fldstruct *fs;         // Pointer to fldstruct var *  
+}; //counterstruct shared;
+
+void * strategy1(void * args)
+{ 
+	struct thread_info *tinfo = args;
+	sleep(5);
+	take(tinfo->fs, 4, 4, tinfo->player);
+
+	return NULL;
+}
+
+void * strategy2(void * args)
+{ 
+	struct thread_info *tinfo = args;
+
+  take(tinfo->fs, 1,1, 1);
+  take(tinfo->fs, 1,2, 2);
+  printfield(tinfo->fs);
+  sleep(1);
+  take(tinfo->fs, 1,3, 3);
+  printfield(tinfo->fs);
+  sleep(1);
+  take(tinfo->fs, 2,2, 4);
+  printfield(tinfo->fs);
+  sleep(1);
+  take(tinfo->fs, 3,2, 5);
+  printfield(tinfo->fs);
+  sleep(1);
+  take(tinfo->fs, 3,3, 6);
+
+	return NULL;
+}
+
+void * strategy3(void * args)
+{ 
+	struct thread_info *tinfo = args;
+	int resval = 0; 
+	int n = tinfo->fs->n;
+
+  for(int i = 0; i < 3; i++)
+  {
+  	for(int y = 0; y < n; y++)
+	  {
+	  	for(int x = 0; x < n; x++)
+	 		{
+	  		resval = take(tinfo->fs, x, y, tinfo->player);
+	  		if (resval == 0)
+	  			break;
+	  		printfield(tinfo->fs);
+	  		usleep(500000);
+	  	}
+	  	if (resval == 0)
+	  		break;
+	  }
+  	if (resval == 0)
+  		break;
+	}
+
+	return NULL;
+}
+
+void * strategy4(void * args)
+{ 
+	struct thread_info *tinfo = args;
+	int resval = 0; 
+	int n = tinfo->fs->n;
+
+  for(int i = 0; i < 3; i++)
+  {
+  	for(int y = 0; y < n; y++)
+	  {
+	  	for(int x = 0; x < n; x++)
+	 		{
+	  		resval = take(tinfo->fs, y, x, tinfo->player);
+	  		if (resval == 0)
+	  			break;
+	  		printfield(tinfo->fs);
+	  		usleep(300000);
+	  	}
+	  	if (resval == 0)
+	  		break;
+	  }
+	}
+
+	return NULL;
+}
+
+void someclients(fldstruct *fs)
+{
+	struct thread_info *tinfo;
+  void  *res;  
+  int nmax , ptcres;
+  nmax = 4;
+
+  // allocate memorey for pthread_creat() arguments
+  tinfo = calloc(nmax, sizeof(struct thread_info));
+  if (tinfo == NULL)
+    err_sys("calloc");
+
+  // Create thread per arg
+  for(int i = 0; i < nmax; i++)
+  {
+    tinfo[i].thread_num = i; 
+    tinfo[i].player = i+1;
+    tinfo[i].fs = fs;
+
+    // the pthread_create() call stores the thread id into corresponding element 
+    // of tinfo[]
+    switch(i % 4)
+    {
+    	case 0:
+    		ptcres = pthread_create(&tinfo[i].thread_id, NULL, &strategy1, &tinfo[i]);
+    		break;
+    	case 1:
+    		ptcres = pthread_create(&tinfo[i].thread_id, NULL, &strategy2, &tinfo[i]);
+    	  break;
+    	case 2:
+    		ptcres = pthread_create(&tinfo[i].thread_id, NULL, &strategy3, &tinfo[i]);
+    	  break;
+    	case 3:
+    	default:
+    		ptcres = pthread_create(&tinfo[i].thread_id, NULL, &strategy4, &tinfo[i]);
+    	
+    	  break;
+    }
+
+    if (ptcres != 0)
+      handle_error(ptcres, "pthread_create", PROCESS_EXIT);
+
+    printf("thread created: %d; \n",
+        tinfo[i].thread_num);
+  }
+
+
+  // join with each thread, and display its returned value */
+  for (int i = 0; i < nmax; i++) {
+    ptcres = pthread_join(tinfo[i].thread_id, &res);
+    if (ptcres != 0)
+        handle_error(ptcres, "pthread_join", PROCESS_EXIT);
+
+    printf("Joined with thread %d; \n",
+            tinfo[i].thread_num /*, (char *) res */);
+    //free(res);      /* Free memory allocated by thread */
+  }
+
+
+  free(tinfo);
+}
+
 int main(int argc, char const *argv[])
 {
 	int n;
@@ -72,37 +226,13 @@ int main(int argc, char const *argv[])
   fldstruct fs;
   initfield(&fs, n);
 
-  take(&fs, 1,1, 1);
-  printfield(&fs);
-  sleep(1);
-  take(&fs, 1,2, 2);
-  printfield(&fs);
-  sleep(1);
-  take(&fs, 1,3, 3);
-  printfield(&fs);
-  sleep(1);
-  take(&fs, 2,2, 4);
-  printfield(&fs);
-  sleep(1);
-  take(&fs, 3,2, 5);
-  printfield(&fs);
-  sleep(1);
-  take(&fs, 3,3, 6);
 
-  int res = 0; 
-  for(int i = 0; i < n; i++)
-  {
-  	for(int j = 0; j < n; j++)
- 		{
-  		res = take(&fs, i,j, 2);
-  		if (res == 0)
-  			break;
-  		printfield(&fs);
-  		sleep(1);
-  	}
-  	if (res == 0)
-  		break;
-  }
+  someclients(&fs);
+
+
+
+
+
 
   printfield(&fs);
 
