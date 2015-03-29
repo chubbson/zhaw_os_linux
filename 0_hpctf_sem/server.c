2d5904d78588bd5f6c19ce7641b8deb9ee6fbc88
had <david.hauri@imis.ch>
2015-03-29 04:30:46 +0200

    init field and print with some color magic :)

---
+++ 0_hpctf_sem/server.c
@@ -0,0 +1,133 @@
+/* (C) Hauronen Patronens waste of time projects!
+ * https://github.com/chubbson/zhaw_os_linux
+ * Author: David Hauri - hauridav
+ * Date: 2015-03-29
+ * License: GPL v2 (See https://de.wikipedia.org/wiki/GNU_General_Public_License )
+**/
+
+#include <apue.h>
+#include <itskylib.h>
+#include <somecolor.h>
+
+void usage(const char *argv)
+{
+	printf("USAGE:\n\n%s fieldsize\n", argv);
+  exit(1);
+} 
+
+typedef struct {
+	int n; // side length
+  int** field; // field
+  pthread_mutex_t mutfield[4][4];
+} fldstruct;
+
+void initfield(fldstruct *fs, int n)
+{
+	fs->n = n;
+  fs->field = malloc(n * sizeof(int*));
+	for (int i = 0; i < n; i++) {
+	  fs->field[i] = malloc(n * sizeof(int));
+	}
+}
+
+void freefield(fldstruct *fs)
+{
+	int n = fs->n;
+	for (int i = 0; i < n; i++) {
+	  free(fs->field[i]);
+	}
+	free(fs->field);
+}
+
+void printcolfield(int i)
+{
+	if(i < 0)
+	{
+		printf( KFWHT "%d" RESET, i*i);
+		return;
+	}
+
+	switch(i%7)
+	{
+		case 1: 
+			printf( KFRED "%d" RESET, i);
+			break;
+		case 2: 
+			printf( KFGRN "%d" RESET, i);
+			break;
+		case 3:  
+			printf( KFYEL "%d" RESET, i);
+			break;
+		case 4: 
+			printf( KFBLU "%d" RESET, i);
+			break;
+		case 5: 
+			printf( KFMAG "%d" RESET, i);
+			break;
+		case 6: 
+			printf( KFCYN "%d" RESET, i);
+			break;
+		case 0: 
+			printf( " " RESET);
+			break; 
+		default: 
+			printf("%d", i);
+			break;
+	} 
+} 
+
+void printfield(fldstruct *fs)
+{
+	// top border
+	for (int i = 0; i < (fs->n+2); ++i)
+	{
+		printcolfield(-1);
+	}
+	printf("\n");
+
+	for (int y = 0; y < fs->n; y++)
+	{
+		// left border
+		printcolfield(-1);
+		for (int x = 0; x < fs->n; x++)
+		{
+			printcolfield(fs->field[y][x]);
+		}
+		// right border 
+		printcolfield(-1);
+		printf("\n");
+	}
+
+	// bottom border
+	for (int i = 0; i < (fs->n+2); ++i)
+	{
+		printcolfield(-1);
+	}
+	printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc < 2 || argc != 2)
+    usage(argv[0]);
+
+  int n = atoi(argv[1]);
+
+  fldstruct fs;
+  initfield(&fs, n);
+
+  fs.field[1][1] = 1; 
+  fs.field[1][2] = 2; 
+  fs.field[1][3] = 3;
+  fs.field[2][2] = 4; 
+  fs.field[3][2] = 5; 
+  fs.field[3][3] = 6; 
+
+  printf("n: %d\n", n);
+  printfield(&fs);
+
+  freefield(&fs);
+
+  exit(0);
+}
+
