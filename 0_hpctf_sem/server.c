af62638ed27dc73aa28a81e6fbad4c531e43ce9a
had <david.hauri@imis.ch>
2015-03-29 10:19:56 +0200

    test take with print

--- 0_hpctf_sem/server.c
+++ 0_hpctf_sem/server.c
@@ -11,7 +11,7 @@
 
 void usage(const char *argv)
 {
-	printf("USAGE:\n\n%s fieldsize\n", argv);
+	printf("USAGE:\n\n%s fieldsizeGreater3\n", argv);
   exit(1);
 } 
  
@@ -23,37 +23,87 @@ typedef struct {
 } fldstruct;
 
 */
-
+int isfinished(fldstruct *fs)
+{
+	int n = fs->n;
+	int res = fs->field[0][0]; 
+	for (int y = 0; y < n; y++)
+	{
+		for (int x = 0; x < n; x++)
+		{
+			if(res != fs->field[y][x])
+				return 0;
+		}
+	}
+	return res;
+}
 
 int take(fldstruct *fs, int y, int x, int player)
 {
+	// todo: mod to phread_mutex_trylock
+	// INUSE\n
+
 	// lock field at x y
 	pthread_mutex_lock(&fs->mutfield[y][x]);
 	fs->field[y][x] = player;
 	// send taken to player
 	// unlock 
 	pthread_mutex_unlock(&fs->mutfield[y][x]);
-	return 0;
+
+	int res = isfinished(fs); 
+	if(res > 0)
+	{
+		printf("END %d \n", res);
+		return 0;
+	} 
+
+	return 1;
 }
 
 int main(int argc, char const *argv[])
 {
-	if (argc < 2 || argc != 2)
+	int n;
+	if (argc < 2 || argc != 2 || (n = atoi(argv[1])) < 4)
     usage(argv[0]);
 
-  int n = atoi(argv[1]);
+  printf("n: %d\n", n);
+
 
   fldstruct fs;
   initfield(&fs, n);
 
   take(&fs, 1,1, 1);
+  printfield(&fs);
+  sleep(1);
   take(&fs, 1,2, 2);
+  printfield(&fs);
+  sleep(1);
   take(&fs, 1,3, 3);
+  printfield(&fs);
+  sleep(1);
   take(&fs, 2,2, 4);
+  printfield(&fs);
+  sleep(1);
   take(&fs, 3,2, 5);
+  printfield(&fs);
+  sleep(1);
   take(&fs, 3,3, 6);
 
-  printf("n: %d\n", n);
+  int res = 0; 
+  for(int i = 0; i < n; i++)
+  {
+  	for(int j = 0; j < n; j++)
+ 		{
+  		res = take(&fs, i,j, 2);
+  		if (res == 0)
+  			break;
+  		printfield(&fs);
+  		sleep(1);
+  	}
+  	if (res == 0)
+  		break;
+  }
+
   printfield(&fs);
 
   freefield(&fs);
